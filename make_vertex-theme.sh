035fa14725f30e60adefd2b3f9157fa24344a8bf
Ildar Akhmetgaleev <akhilman@gmail.com>
2016-06-01 07:35:16 +0700

    vertex theme

---
+++ make_vertex-theme.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -d vertex-theme ]
+then
+    (
+    cd vertex-theme
+    git pull
+    )
+else
+    git clone https://github.com/horst3180/vertex-theme
+fi
+
+if [ -d /usr/share/themes/Vertex ] || \
+    [ -d /usr/share/themes/Vertex-Dark ] || \
+    [ -d /usr/share/themes/Vertex-Light ]
+then
+    echo Please remove old package first
+    exit 1
+fi
+
+
+cd vertex-theme
+
+cat << EOF > description-pak
+Vertex is a theme for GTK 3, GTK 2, Gnome-Shell and Cinnamon. It supports GTK 3 and GTK 2 based desktop environments like Gnome, Cinnamon, Mate, XFCE, Budgie, Pantheon, etc.
+EOF
+
+./autogen.sh --prefix=/usr
+make
+fakeroot checkinstall \
+    --pkgname=vertex-theme \
+    --pkgversion=$(date +%Y.%m.%d) \
+    --pkgarch=all \
+    --pkgsource="https://github.com/horst3180/vertex-theme" \
+    --provides=vertext-theme \
+    --nodoc  \
+    --default \
+    make install
+
