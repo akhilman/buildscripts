e63bcb15d4e98f5a6cc83913c6e520f39590652c
Ildar Akhmetgaleev <akhilman@gmail.com>
2016-06-19 09:04:54 +0700

    adding make_cjdns_deb.sh

---
+++ make_cjdns_deb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+to_install=
+for p in git build-essential dh-systemd nodejs
+do
+    dpkg-query -l $p || to_install="$to_install $p" 
+done
+if [ ! -z "$to_install" ]
+then
+    sudo apt-get install $to_install
+fi
+
+cd $(dirname $0)
+mkdir cjdns; cd cjdns
+
+if [ -x cjdns ]
+then
+    (cd cjdns; git pull)
+else
+    git clone https://github.com/cjdelisle/cjdns.git cjdns
+fi
+
+(cd cjdns; ./clean; dpkg-buildpackage -tc -rfakeroot -uc -b)
+
+sudo dpkg -i $(ls cjdns_*_*.deb | tail -n 1)
